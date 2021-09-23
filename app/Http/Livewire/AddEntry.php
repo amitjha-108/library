fe7b42f02354d26a8f1933e85deb5a1742b6fa4e
Amit <108jhaamit@gmail.com>
2021-09-23 11:29:54 +0530

    Library first commit

---
+++ app/Http/Livewire/AddEntry.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Livewire;
+use App\Models\Book;
+use App\Models\User;
+use Livewire\Component;
+
+class AddEntry extends Component
+{
+    public $books, $students,$rfcode,$isbn,$qty,$total_amount,$membership;
+
+    public function mount(){
+        $this->books = NULL;
+        $this->students = NULL;
+        $this->qty = 1;
+        
+    }
+
+    public function find(){
+        $this->students = User::where("rfcode",$this->rfcode)->first();
+    }
+
+    public function isbnFind(){
+        $this->books = Book::where("isbn",$this->isbn)->first();
+        $this->total_amount = $this->books->rent_price;
+
+    }
+
+    public function qtyUp(){
+        $this->total_amount = $this->qty * $this->books->rent_price;
+    }
+    public function render()
+    {
+        return view('livewire.add-entry',["total"=>$this->total_amount]);
+    }
+
+}
